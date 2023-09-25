585326f41abaf75b5342a5de8a25529ef3c92902
sajibsd013 <sajibsd013@gmail.com>
2023-09-25 11:28:51 +0600

    admob

--- App.js
+++ App.js
@@ -1,61 +1,216 @@
-import React from "react";
+import React, { Component } from "react";
 import { WebView } from "react-native-webview";
-import { BackHandler, Alert } from "react-native";
-import InternetConnectionAlert from "react-native-internet-connection-alert";
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.WEBVIEW_REF = React.createRef();
-  }
+import { BackHandler, Alert, ScrollView, RefreshControl } from "react-native";
+import "expo-dev-client";
+import {
+  InterstitialAd,
+  AdEventType,
+  BannerAd,
+  BannerAdSize,
+  RewardedAd,
+  RewardedAdEventType,
+  TestIds,
+  AppOpenAd,
+} from "react-native-google-mobile-ads";
 
-  componentDidMount() {
-    BackHandler.addEventListener("hardwareBackPress", this.handleBackButton);
-  }
+import InterstitialAdsScreen from "./Ads/InterstitialAdsScreen";
 
-  componentWillUnmount() {
-    BackHandler.removeEventListener("hardwareBackPress", this.handleBackButton);
-  }
+const adUnitIdInterstitial = __DEV__
+  ? TestIds.INTERSTITIAL
+  : "ca-app-pub-3545205458668722/1332006425";
 
-  handleBackButton = () => {
-    this.WEBVIEW_REF.current.goBack();
+const adUnitIdBanner = __DEV__
+  ? TestIds.BANNER
+  : "ca-app-pub-3545205458668722/4857932864";
+
+const adUnitIdrewarded = __DEV__
+  ? TestIds.REWARDED
+  : "ca-app-pub-3545205458668722/5447630813";
+
+const adUnitId = __DEV__
+  ? TestIds.APP_OPEN
+  : "ca-app-pub-xxxxxxxxxxxxx/yyyyyyyyyyyyyy";
+
+export default class App extends Component {
+  webView = {
+    ref: null,
+  };
+  state = {
+    canGoBack: false,
+    url: "",
+    enablePTR: false,
+    interstitial_loaded: false,
+    loaded: false,
+  };
+  _OnScroll(e) {}
+
+  interstitial = InterstitialAd.createForAdRequest(adUnitIdInterstitial, {
+    requestNonPersonalizedAdsOnly: true,
+    keywords: ["fashion", "clothing"],
+  });
+
+  rewarded = RewardedAd.createForAdRequest(adUnitIdrewarded, {
+    requestNonPersonalizedAdsOnly: true,
+    keywords: ["fashion", "clothing"],
+  });
+
+  // appOpenAd = AppOpenAd.createForAdRequest(adUnitId, {
+  //   requestNonPersonalizedAdsOnly: true,
+  //   keywords: ["fashion", "clothing"],
+  // });
+
+  onAndroidBackPress = () => {
+    // console.log(this.state.url);
+    this.handleInterstitialShow();
+    if (this.state.url == "https://sasthosebok.com/") {
+      this.handlerewardedShow();
+
+      Alert.alert("Close this app?", "Are you sure, You want to exit?", [
+        {
+          text: "No",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel",
+        },
+        { text: "Yes", onPress: () => BackHandler.exitApp() },
+      ]);
+    } else {
+      this.webView.ref.goBack();
+    }
     return true;
   };
 
-  onNavigationStateChange(navState) {
-    this.setState({
-      canGoBack: navState.canGoBack,
-    });
+  handleInterstitialShow = () => {
+    if (this.state.interstitial_loaded) {
+      this.interstitial.show();
+      this.setState({ interstitial_loaded: false }); // Reset interstitial_loaded state
+      this.interstitial.load(); // Reload the ad
+    }
+  };
+  handlerewardedShow = () => {
+    if (this.state.loaded) {
+      this.rewarded.show();
+      this.setState({ loaded: false }); // Reset loaded state
+      this.rewarded.load(); // Reload the ad
+    }
+  };
+
+  componentDidMount() {
+    BackHandler.addEventListener("hardwareBackPress", this.onAndroidBackPress);
+
+    this.unsubscribe = this.interstitial.addAdEventListener(
+      AdEventType.LOADED,
+      () => {
+        this.setState({ interstitial_loaded: true });
+      }
+    );
+
+    // Start loading the interstitial straight away
+
+    this.unsubscribeLoaded = this.rewarded.addAdEventListener(
+      RewardedAdEventType.LOADED,
+      () => {
+        this.setState({ loaded: true });
+      }
+    );
+    this.unsubscribeEarned = this.rewarded.addAdEventListener(
+      RewardedAdEventType.EARNED_REWARD,
+      (reward) => {
+        console.log("User earned reward of ", reward);
+      }
+    );
+
+    // Start loading the rewarded ad straight away
+    // Preload an app open ad
+    
+
+    // Show the app open ad when user brings the app to the foreground.
+    // if(this.appOpenAd.load()){
+    //   this.appOpenAd.show();
+    // }
+
+    
+    this.rewarded.load();
+    this.interstitial.load();
+  }
+
+  componentWillUnmount() {
+    BackHandler.removeEventListener("hardwareBackPress");
+    // Unsubscribe from events on unmount
+    if (this.unsubscribe) {
+      this.unsubscribe();
+    }
+    if (this.unsubscribeLoaded) {
+      this.unsubscribeLoaded();
+    }
+    if (this.unsubscribeEarned) {
+      this.unsubscribeEarned();
+    }
   }
 
   render() {
-    const WebApp = () => {
-      return (
+    return (
+      <ScrollView
+        style={{
+          backgroundColor: "white",
+          position: "relative",
+        }}
+        contentContainerStyle={{ flex: 1 }}
+        refreshControl={
+          <RefreshControl
+            enabled={this.state.enablePTR}
+            refreshing={false}
+            onRefresh={() => this.webView.ref.reload()} // exl in function : this.yourWebview.reload();
+          />
+        }
+      >
+        <BannerAd
+          unitId={adUnitIdBanner}
+          size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+          requestOptions={{
+            requestNonPersonalizedAdsOnly: true,
+          }}
+        />
+        {/* <InterstitialAdsScreen /> */}
         <WebView
+          onScroll={(e) => {
+            console.log(e.nativeEvent.contentOffset.y);
+            const yAxis = e.nativeEvent.contentOffset.y;
+            if (yAxis === 0) {
+              this.setState({ enablePTR: true });
+            } else {
+              this.setState({ enablePTR: false });
+            }
+          }}
           javaScriptEnabled={true}
           domStorageEnabled={true}
           cacheEnabled={true}
           thirdPartyCookiesEnabled={true}
           setBuiltInZoomControls={false}
           source={{ uri: "https://sasthosebok.com/" }}
-          ref={this.WEBVIEW_REF}
-          // onNavigationStateChange={this.onNavigationStateChange.bind(this)}
+          ref={(webView) => {
+            this.webView.ref = webView;
+          }}
           onError={(event) => {
             Alert.alert(
               "Your are Offline!",
               "Please check your network connection",
-              [{ text: "Ok", onPress: () => console.log("Your are Offline ") }]
+              [
+                {
+                  text: "Ok",
+                  onPress: () => console.log("Ok"),
+                },
+              ]
             );
           }}
+          onLoadProgress={(nativeEvent) => {
+            this.setState({
+              canGoBack: nativeEvent.nativeEvent.canGoBack,
+              url: nativeEvent.nativeEvent.url,
+            });
+          }}
+          onNavigationStateChange={(navState) => {}}
         />
-      );
-    };
-
-    return (
-      <InternetConnectionAlert onChange={(connectionState) => {}}>
-        <WebApp />
-      </InternetConnectionAlert>
+      </ScrollView>
     );
   }
 }
-
-// eas build -p android --profile preview
---
+++ Readme.md
@@ -0,0 +1,6 @@
+eas build --profile development --platform android
+
+
+eas build --platform android 
+npx expo start
+eas build -p android --profile preview2
